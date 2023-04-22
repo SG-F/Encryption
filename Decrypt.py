786e1b7dcb3069a1dc1ed9759d2891f3ee1dd79f
SG-F <42728871+SG-F@users.noreply.github.com>
2023-04-22 17:25:07 +0200

    Add files via upload

---
+++ Decrypt.py
@@ -0,0 +1,23 @@
+list=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+c=0
+k=t=0
+z=""
+text=input("Enter Cipher text ")
+key=input("Enter The Key ")
+for i in range(len(text)):
+    if text[t]==' ':
+        z= z +' '
+        t +=1
+    elif k != len(key):
+        c=((list.index(text[t]))-list.index(key[k]))%26
+        t +=1
+        k +=1
+        z = z+ list[c]
+    else:
+        k=0
+        c=((list.index(text[t]))-list.index(key[k]))%26
+        k+=1
+        t+=1
+        z = z+ list[c]
+
+print (z)
